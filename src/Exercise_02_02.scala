deb9a523ccd2cea78e2cdab4206ae7dc0782217e
Michael <michael.s.kwon@gmail.com>
2018-07-22 21:32:42 -0700

    Add Exercise 2.2 code

--- src/Exercise_02_02.scala
+++ src/Exercise_02_02.scala
@@ -1,1 +1,21 @@
+object isSortedExercise {
+  def isSorted[A](as: Array[A], ordered: (A, A) => Boolean): Boolean = {
+    @annotation.tailrec
+    def loop(n: Int): Boolean =
+      if (n+1 >= as.length) true
+      else if (!ordered(as(n), as(n+1))) false
+      else loop(n+1)
 
+    loop(0)
+  }
+
+  private def formatIsSorted[A](as: Array[A], ordered: (A, A) => Boolean) =
+    s"[${as.mkString(" ")}] is ${if (isSorted(as, ordered)) "" else "not "}sorted."
+
+  def main(args: Array[String]): Unit = {
+    println(formatIsSorted(Array(1,2,3), (x: Int, y: Int) => x <= y))
+    println(formatIsSorted(Array(2,1,3), (x: Int, y: Int) => x <= y))
+    println(formatIsSorted(Array("A", "B", "C"), (x: String, y: String) => x <= y))
+    println(formatIsSorted(Array("A", "C", "B"), (x: String, y: String) => x <= y))
+  }
+}
