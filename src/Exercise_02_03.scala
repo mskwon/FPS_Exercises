fa84c1bfb4d0a842827b0c4b3e72d0e3eed7c06d
Michael <michael.s.kwon@gmail.com>
2018-07-22 21:42:44 -0700

    Add code for Exercise 2.3

--- src/Exercise_02_03.scala
+++ src/Exercise_02_03.scala
@@ -1,1 +1,13 @@
+object CurryingExercise {
+  def curry[A, B, C](f: (A, B) => C): A => (B => C) =
+    (a: A) => (b: B) => f(a, b)
 
+  def main(args: Array[String]): Unit = {
+    def function(x: Int, y: Int): Int =
+      x + y
+
+    var curried_func = curry(function)
+    var intermediate_func = curried_func(3)
+    println(intermediate_func(2))
+  }
+}
