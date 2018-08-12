d82530441030a9be97dfa7fbc7c5a027744122f1
Michael <michael.s.kwon@gmail.com>
2018-08-11 21:44:27 -0700

    Move scripts to individual directories to more easily exclude them in order to avoid interference by sealed traits on default types

--- src/Chapter_3/Chapter_3.scala
+++ src/Chapter_3/Chapter_3.scala
@@ -1,6 +1,6 @@
-//sealed trait List[+A]
-//case object Nil extends List[Nothing]
-//case class Cons[+A](head: A, tail: List[A]) extends List[A]
+sealed trait List[+A]
+case object Nil extends List[Nothing]
+case class Cons[+A](head: A, tail: List[A]) extends List[A]
 
 object List {
   def sum(ints: List[Int]): Int = ints match {
