5647cb0bdbed119f6c92b8bf3432ec4518420d0c
Michael <michael.s.kwon@gmail.com>
2018-08-13 07:32:59 -0700

    Add solution for Exercise 5.2

--- src/Chapter_5/Chapter_5.scala
+++ src/Chapter_5/Chapter_5.scala
@@ -12,6 +12,13 @@ sealed trait Stream[+A] {
     case Empty => Nil
     case Cons(h, t) => h() :: t().toList
   }
+
+  // Exercise 5.2
+  def take(n: Int): Stream[A] = this match {
+    case Empty => Empty
+    case Cons(h, t) if n > 0 => Cons(h, () => t().take(n - 1))
+    case _ => Stream[A]()
+  }
 }
 
 object Stream {
@@ -27,12 +34,19 @@ object Stream {
 }
 
 object Chapter_5{
+  // Exercise 5.1
   def ex_5_1(): Unit = {
     val testStream = Stream(1, 2, 3, 4)
     println(testStream.toList)
   }
 
+  def ex_5_2(): Unit = {
+    val testStream = Stream(1, 2, 3, 4, 5, 6)
+    println(testStream.take(3).toList)
+  }
+
   def main(args: Array[String]): Unit = {
-    ex_5_1()
+    //ex_5_1()
+    ex_5_2()
   }
 }
