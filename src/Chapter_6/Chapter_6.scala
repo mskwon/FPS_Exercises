bd0a7751ebd1cfa114c3966932ac7d790c10e9c6
Michael <michael.s.kwon@gmail.com>
2018-08-25 17:04:23 -0700

    Add solution for Exercise 6.2

--- src/Chapter_6/Chapter_6.scala
+++ src/Chapter_6/Chapter_6.scala
@@ -33,7 +33,26 @@ object Chapter_6 {
     println(randomInt(rng_3))
   }
 
+  // Exercise 6.2
+  def double(rng: RNG): (Double, RNG) = {
+    val (i1, next_rng) = randomInt(rng)
+
+    if (i1 == Int.MaxValue) double(next_rng)
+    else (i1.toDouble / Int.MaxValue, next_rng)
+  }
+
+  def ex_6_2(): Unit = {
+    val rng_1 = SimpleRNG(0)
+    val (_, rng_2) = double(rng_1)
+    val (_, rng_3) = double(rng_2)
+
+    println(double(rng_1))
+    println(double(rng_2))
+    println(double(rng_3))
+  }
+
   def main(args: Array[String]): Unit = {
-    ex_6_1()
+    //ex_6_1()
+    ex_6_2()
   }
 }
