a9b3da042e3e604cb20050ef3302e4fa136a4372
Michael <michael.s.kwon@gmail.com>
2018-08-26 17:06:00 -0700

    Add solution for Exercise 6.5

--- src/Chapter_6/Chapter_6.scala
+++ src/Chapter_6/Chapter_6.scala
@@ -1,4 +1,3 @@
-
 trait RNG {
   def nextInt: (Int, RNG)
 }
@@ -10,32 +9,39 @@ case class SimpleRNG(seed: Long) extends RNG {
     val n = (newSeed >>> 16).toInt
     (n, nextRNG)
   }
+
+  type Rand[+A] = RNG => (A, RNG)
+
+  val int: Rand[Int] = _.nextInt
+
+  def unit[A](a: A): Rand[A] =
+    rng => (a, rng)
 }
 
 object Chapter_6 {
   // Exercise 6.1
-  def randomInt(rng: RNG): (Int, RNG) = {
+  def nonNegativeInt(rng: RNG): (Int, RNG) = {
     val (i1, next_rng) = rng.nextInt
 
-    if (i1 == Int.MinValue) randomInt(next_rng)
+    if (i1 == Int.MinValue) nonNegativeInt(next_rng)
     else (math.abs(i1), next_rng)
   }
 
   def ex_6_1(): Unit = {
     val rng_1 = SimpleRNG(0)
-    val (_, rng_2) = randomInt(rng_1)
-    val (_, rng_3) = randomInt(rng_2)
+    val (_, rng_2) = nonNegativeInt(rng_1)
+    val (_, rng_3) = nonNegativeInt(rng_2)
 
     println("IntMax: %d".format(Int.MaxValue))
     println("IntMin: %d".format(Int.MinValue))
-    println(randomInt(rng_1))
-    println(randomInt(rng_2))
-    println(randomInt(rng_3))
+    println(nonNegativeInt(rng_1))
+    println(nonNegativeInt(rng_2))
+    println(nonNegativeInt(rng_3))
   }
 
   // Exercise 6.2
   def double(rng: RNG): (Double, RNG) = {
-    val (i1, next_rng) = randomInt(rng)
+    val (i1, next_rng) = nonNegativeInt(rng)
 
     if (i1 == Int.MaxValue) double(next_rng)
     else (i1.toDouble / Int.MaxValue, next_rng)
@@ -53,7 +59,7 @@ object Chapter_6 {
 
   // Exercise 6.3
   def intDouble(rng: RNG): ((Int,Double), RNG) = {
-    val (i, rng_1) = randomInt(rng)
+    val (i, rng_1) = nonNegativeInt(rng)
     val (d, rng_2) = double(rng_1)
 
     ((i, d), rng_2)
@@ -83,7 +89,7 @@ object Chapter_6 {
 
   // Exercise 6.4
   def ints(count: Int)(rng: RNG): (List[Int], RNG) = {
-    val (i, rng_1) = randomInt(rng)
+    val (i, rng_1) = nonNegativeInt(rng)
 
     if (count == 0) (Nil, rng)
     else if (count == 1) (List(i), rng_1)
@@ -99,10 +105,31 @@ object Chapter_6 {
     println(ints(5)(rng))
   }
 
+  // Exercise 6.5
+  type Rand[+A] = RNG => (A, RNG)
+
+  def map[A,B](s: Rand[A])(f: A => B): Rand[B] =
+    rng => {
+      val (a, rng2) = s(rng)
+      (f(a), rng2)
+    }
+
+  def double_2: Rand[Double] =
+    map(nonNegativeInt)(i => i match {
+      case Int.MaxValue => (i - 1).toDouble / Int.MaxValue
+      case _ => i.toDouble / Int.MaxValue
+    })
+
+  def ex_6_5(): Unit = {
+    val rng = SimpleRNG(11)
+    println(double_2(rng))
+  }
+
   def main(args: Array[String]): Unit = {
     //ex_6_1()
     //ex_6_2()
     //ex_6_3()
-    ex_6_4()
+    //ex_6_4()
+    ex_6_5()
   }
 }
